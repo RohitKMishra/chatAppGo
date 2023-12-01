45c33f9958ae02a1314f82e6cb17321663c639d8
Rohit Kumar Mishra <rohitmishra1499769@gmail.com>
2023-12-01 22:09:54 +0530

    moved models.go out from models package

--- user_store.go
+++ user_store.go
@@ -3,36 +3,34 @@ package main
 
 import (
 	"sync"
-
-	"github.com/RohitKMishra/chatAppGo/models"
 )
 
 type UserStore interface {
-	Store(userID string, user models.User)
-	Load(userID string) (user models.User, ok bool)
+	Store(userID string, user User)
+	Load(userID string) (user User, ok bool)
 	Delete(userID string)
 }
 
 type InMemoryUserStore struct {
 	sync.Mutex
-	users map[string]models.User
+	users map[string]User
 }
 
 var _ UserStore = (*InMemoryUserStore)(nil)
 
 func NewInMemoryUserStore() *InMemoryUserStore {
 	return &InMemoryUserStore{
-		users: map[string]models.User{},
+		users: map[string]User{},
 	}
 }
 
-func (s *InMemoryUserStore) Store(userID string, user models.User) {
+func (s *InMemoryUserStore) Store(userID string, user User) {
 	s.Lock()
 	s.users[userID] = user
 	s.Unlock()
 }
 
-func (s *InMemoryUserStore) Load(userID string) (user models.User, ok bool) {
+func (s *InMemoryUserStore) Load(userID string) (user User, ok bool) {
 	s.Lock()
 	user, ok = s.users[userID]
 	s.Unlock()
